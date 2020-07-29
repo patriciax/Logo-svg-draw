0ad5ca20ed0044e57bf5061f8b91f150366e090c
patriciax <yively.pa@gmail.com>
2020-07-29 07:50:47 -0400

    logo test

---
+++ script.js
@@ -0,0 +1,11 @@
+
+new Vivus('logo', {}, function (obj) {
+  start: 'autostart',
+  obj.el.classList.add('finished');
+});
+
+
+
+new Vivus('logo-txt', {}, function (obj) {
+  obj.el.classList.add('finished');
+});
